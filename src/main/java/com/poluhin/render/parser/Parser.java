ca031ccbfd94e68c9cc1d2a9a6fa964504fa0cc9
Sergey Poluhin <sergey.poluhin@spbpu.com>
2019-01-18 12:38:19 +0300

    Create a simple parser

---
+++ src/main/java/com/poluhin/render/parser/Parser.java
@@ -0,0 +1,25 @@
+package com.poluhin.render.parser;
+
+import com.poluhin.render.engine.structures.Point;
+import com.poluhin.render.engine.structures.Polygon;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Parser {
+    public static List<Polygon> parse(String pathToFile) throws IOException {
+        return Files.readAllLines(Paths.get(pathToFile))
+                .stream()
+                .map(line -> {
+                    var parts = Arrays.stream(line.split("\\s*,\\s*")).map(Integer::parseInt).toArray(Integer[]::new);
+                    return new Polygon(new Point(parts[0], parts[1], parts[2]),
+                            new Point(parts[3], parts[4], parts[5]),
+                            new Point(parts[6], parts[7], parts[8]));
+                })
+                .collect(Collectors.toList());
+    }
+}
