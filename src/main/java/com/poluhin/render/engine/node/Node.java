d7099773441446f817203ac1a56e45512ef2a2dc
Сергей Полухин <s.poluh@ro.ru>
2019-01-20 01:59:45 +0300

    Create Node-class

---
+++ src/main/java/com/poluhin/render/engine/node/Node.java
@@ -0,0 +1,57 @@
+package com.poluhin.render.engine.node;
+
+import java.util.Set;
+
+public class Node {
+    private Set<Point> points;
+    private Point rotationCenter;
+    private double depth;
+
+    public Node(Set<Point> points) {
+        this.points = points;
+        depth = 0;
+        rotationCenter = getRotationCenter();
+    }
+
+    public Node(Set<Point> points, double depth) {
+        this.points = points;
+        this.depth = depth;
+        rotationCenter = getRotationCenter();
+    }
+
+    public Node(Set<Point> points, Point rotationCenter, double depth) {
+        this.points = points;
+        this.rotationCenter = rotationCenter;
+        this.depth = depth;
+    }
+
+    public Set<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(Set<Point> points) {
+        this.points = points;
+    }
+
+    public Point getRotationCenter() {
+        if (rotationCenter == null) {
+            var x = points.stream().map(Point::getX).mapToInt(Integer::new).sum() / points.size();
+            var y = points.stream().map(Point::getY).mapToInt(Integer::new).sum() / points.size();
+            var z = points.stream().map(Point::getZ).mapToInt(Integer::new).sum() / points.size();
+            return new Point(x, y, z);
+        }
+        return rotationCenter;
+    }
+
+    public void setRotationCenter(Point rotationCenter) {
+        this.rotationCenter = rotationCenter;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+}
